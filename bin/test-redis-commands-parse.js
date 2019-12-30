e9e4f2596ece2c7ba96fce8318bf4bd2a2dd67d8
ryeon9445 <ryeon9445@gmail.com>
2019-12-30 12:39:33 +0900

    redis commends parse 주석 추가

--- bin/test-redis-commands-parse.js
+++ bin/test-redis-commands-parse.js
@@ -9,25 +9,40 @@ const requestPromise = require("request-promise");
   for (let i = 0; i < commands.length; i++) {
     const cmd = commands[i];
 
+    /**
+     * 명령어
+     */
     const command = $(".command", cmd)
       .html()
       .trim()
       .replace(/^\s+.+/gm, "")
       .trim();
 
+    /**
+     * 명령어에 필요한 아규먼트
+     */
     const args = $(".command .args", cmd)
       .text()
       .trim()
       .replace(/\s+/g, " ");
 
+    /**
+     * 명령어의 간략한 설명
+     */
     const summary = $(".summary", cmd)
       .text()
       .trim();
 
+    /**
+     * 명령어, 아규먼트, 명령어 설명 출력
+     */
     console.log(command + "," + args + "," + summary);
   }
 })();
 
+/**
+ * 레디스 명령어목록 페이지 html 정보를 가져온다.
+ */
 async function getDocument() {
   const options = Object.assign({
     method: "GET",
